41a6b6c4bf70558fb22c6b2d4d2d0219c6b55c3e
Sammy Akharaz <ketrixz@gmail.com>
2018-05-03 15:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var obj = {itemName: item, itemPrice: Math.floor((Math.random() * 100) + 1)}
+  cart.push(obj)
+  return `${obj["itemName"]} has been added to your cart.`
 }
-
+/*
+2. The `viewCart()` function does not accept any arguments. It should loop over every item in your cart, returning the contents as one long, coherent statement in this format: `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+    + If the cart is empty, the function should instead return `Your shopping cart is empty.`
+    + ***Note***: Pay close attention to the syntax above. The returned statement should be a single sentence that begins with `In your cart, you have `, terminates in a period, and can assume the following shapes according to how many items the cart contains:
+      * 1 item — `In your cart, you have bananas at $17.`
+      * 2 items — `In your cart, you have bananas at $17, and pancake batter at $5.`
+      * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+*/
 function viewCart() {
-  // write your code here
+  for(let i = 0; i<cart.length; i++){
+    cart[i][]
+  }
 }
 
 function total() {
